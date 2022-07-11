d1f2c1d9a5ea149c1869d2efb5d46d45bea587c5
Filip Nilsson <filip.c.nilsson@gmail.com>
2022-07-11 12:07:46 +0200

    better ouput

--- task_a/code/util.py
+++ task_a/code/util.py
@@ -1,11 +1,11 @@
 def create_output(model, tokenizer, train_data, val_data, learning_rate, epochs, batch_size, output_file):
     f = open("../outputs/" + output_file, 'a')
     f.write("## Info\n")
-    f.write(f"This was trained on ")
+    f.write(f"This was trained on:\n")
     for i in train_data:
-        f.write(f"[{i}](https://github.com/flippe3/fire_2022/tree/master/task_a/data/{i})")
+        f.write(f"- [{i}](https://github.com/flippe3/fire_2022/tree/master/task_a/data/{i})\n")
 
-    f.write(f" and validated on [{val_data}](https://github.com/flippe3/fire_2022/tree/master/task_a/data/{val_data})\n\n")
+    f.write(f"\nvalidated on:\n - [{val_data}](https://github.com/flippe3/fire_2022/tree/master/task_a/data/{val_data})\n\n")
 
     f.write(f"Model: [{model}](https://huggingface.co/{model})\n\n Tokenizer: [{tokenizer}](https://huggingface.co/{tokenizer})\n\n")
     
