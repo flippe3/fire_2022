3b1ec340bbcd1d670ba86d7a3803922c3232edf5
Filip Nilsson <filip.c.nilsson@gmail.com>
2022-08-13 09:48:24 +0000

    MTL

--- MTL/train.py
+++ MTL/train.py
@@ -31,12 +31,22 @@ model_name = "sentence-transformers/paraphrase-xlm-r-multilingual-v1"
 multitask_model = MultitaskModel.create(
     model_name=model_name,
     model_type_dict={
-        "sentiment": transformers.AutoModelForSequenceClassification,
-        "phobia": transformers.AutoModelForSequenceClassification
+        "kan_sentiment": transformers.AutoModelForSequenceClassification,
+        "mal_sentiment": transformers.AutoModelForSequenceClassification,
+        "tam_sentiment": transformers.AutoModelForSequenceClassification,
+        "eng_phobia": transformers.AutoModelForSequenceClassification,
+        "tam_phobia": transformers.AutoModelForSequenceClassification,
+        "mal_phobia": transformers.AutoModelForSequenceClassification,
+        "eng_tam_phobia": transformers.AutoModelForSequenceClassification
     },
     model_config_dict={
-        "sentiment": transformers.AutoConfig.from_pretrained(model_name, num_labels=5),
-        "phobia": transformers.AutoConfig.from_pretrained(model_name, num_labels=3)
+        "kan_sentiment": transformers.AutoConfig.from_pretrained(model_name, num_labels=5),
+        "mal_sentiment": transformers.AutoConfig.from_pretrained(model_name, num_labels=5),
+        "tam_sentiment": transformers.AutoConfig.from_pretrained(model_name, num_labels=5),
+        "eng_phobia": transformers.AutoConfig.from_pretrained(model_name, num_labels=3),
+        "tam_phobia": transformers.AutoConfig.from_pretrained(model_name, num_labels=3),
+        "mal_phobia": transformers.AutoConfig.from_pretrained(model_name, num_labels=3),
+        "eng_tam_phobia": transformers.AutoConfig.from_pretrained(model_name, num_labels=3)
     },
 )
 
@@ -54,58 +64,70 @@ dataset_dict = {
 }
 
 def convert_to_features(example_batch):
-    print(example_batch['text'][0])
     features = {}
-    for text in example_batch['text']:
-        features['inputs'].append(tokenizer.encode_plus(
-                                    text,            
+    features = tokenizer.batch_encode_plus(
+                                    example_batch['text'],            
                                     add_special_tokens = True,
                                     max_length = 512,
                                     padding = 'max_length',
                                     return_attention_mask = True,
-                                    truncation=True,
-                                    return_tensors = 'pt'))
+                                    truncation=True)
+#                                    return_tensors = 'pt')
         
     features["labels"] = example_batch["category"]
-    print(type(features))
     return features
 
 convert_func_dict = {
     "kan_sentiment": convert_to_features,
     "mal_sentiment": convert_to_features,
     "tam_sentiment": convert_to_features,
-    "phobia": convert_to_features,
+    "eng_phobia": convert_to_features,
+    "tam_phobia": convert_to_features,
+    "mal_phobia": convert_to_features,
+    "eng_tam_phobia": convert_to_features,
 }
 
 columns_dict = {
-    "sentiment": ['input_ids', 'attention_mask', 'labels'],
-    "phobia": ['input_ids', 'attention_mask', 'labels'],
+    "kan_sentiment": ['input_ids', 'attention_mask', 'labels'],
+    "mal_sentiment": ['input_ids', 'attention_mask', 'labels'],
+    "tam_sentiment": ['input_ids', 'attention_mask', 'labels'],
+    
+    "eng_phobia": ['input_ids', 'attention_mask', 'labels'],
+    "tam_phobia": ['input_ids', 'attention_mask', 'labels'],
+    "mal_phobia": ['input_ids', 'attention_mask', 'labels'],
+    "eng_tam_phobia": ['input_ids', 'attention_mask', 'labels'],
 }
-
-if model_name.startswith("sen"):
-    print(multitask_model.encoder.embeddings.word_embeddings.weight.data_ptr())
-    print(multitask_model.taskmodels_dict["sentiment"].roberta.embeddings.word_embeddings.weight.data_ptr())
-    print(multitask_model.taskmodels_dict["phobia"].roberta.embeddings.word_embeddings.weight.data_ptr())
-    print("Architecture maybe works?")
-else:
-    print("Exercise for the reader: add a check for other model architectures =)")
-
-
-#print(dataset_dict.items())
 
 features_dict = {}
 for task_name, dataset in dataset_dict.items():
     features_dict[task_name] = {}
     for phase, phase_dataset in dataset.items():
-        print(phase_dataset)
         features_dict[task_name][phase] = phase_dataset.map(
             convert_func_dict[task_name],
             batched=True,
             load_from_cache_file=False,
         )
-        print(task_name, phase, len(phase_dataset), len(features_dict[task_name][phase]))
         features_dict[task_name][phase].set_format(
             type="torch", 
             columns=columns_dict[task_name],
         )
         print(task_name, phase, len(phase_dataset), len(features_dict[task_name][phase]))
+
+train_dataset = {
+		task_name: dataset["train"] for task_name, dataset in features_dict.items()
+}
+trainer = MultitaskTrainer(
+    model=multitask_model,
+    args=transformers.TrainingArguments(
+        output_dir="output_trainer",
+        overwrite_output_dir=True,
+        learning_rate=3e-5,
+        do_train=True,
+        num_train_epochs=3,
+        per_device_train_batch_size=16,
+        save_steps=3000,
+    ),
+    data_collator=NLPDataCollator(),
+    train_dataset=train_dataset,
+)
+trainer.train()
